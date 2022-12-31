975d9c4db6610db0a47d4b19b33964dfdc76b573
jose luis <2064537+sosan@users.noreply.github.com>
2022-12-31 17:59:10 +0100

    comments

--- service-mesh-vs-ingress/init/foreground.sh
+++ service-mesh-vs-ingress/init/foreground.sh
@@ -1,10 +1,10 @@
-echo "========= INSTALING ISTIO ========="
+# ========= INSTALING ISTIO =========
 FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
 export ISTIO_VERSION=1.16.1
 curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
 echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
-echo "========= SETTING PATH ========="
+# ========= SETTING PATH =========
 export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
 mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
 istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
-echo "========= INSTALLED ========="
+# ========= INSTALLED =========
