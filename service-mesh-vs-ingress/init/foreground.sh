6b1f264155d3546503080715e866d8fcf512a699
jose luis <2064537+sosan@users.noreply.github.com>
2023-01-01 00:52:24 +0100

    correct position for installing istio

--- service-mesh-vs-ingress/init/foreground.sh
+++ service-mesh-vs-ingress/init/foreground.sh
@@ -1,4 +1,4 @@
-# ========= INSTALING ISTIO =========
+#!
 FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
 export ISTIO_VERSION=1.16.1
 curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
@@ -6,5 +6,6 @@ echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
 # ========= SETTING PATH =========
 export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
 mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
+# ========= INSTALING ISTIO =========
 istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
 # ========= INSTALLED =========
