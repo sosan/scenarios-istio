af62ab89f32894c6410424b0044bc2e79ae60274
jose luis <2064537+sosan@users.noreply.github.com>
2023-01-01 20:28:43 +0100

    changed text explication in wait-headers.sh

--- service-mesh-vs-ingress/assets/labs/01/wait-headers.sh
+++ service-mesh-vs-ingress/assets/labs/01/wait-headers.sh
@@ -1,7 +1,7 @@
 #!
-echo "Waiting..."
-until kubectl -n envoy_lab_01 exec deploy/sleep -- curl -s --max-time 1 http://envoy/headers >/dev/null 2>&1
+echo "Waiting to getting up..."
+until kubectl -n envoy-lab-01 exec deploy/sleep -- curl -s --max-time 1 http://envoy/headers >/dev/null 2>&1
 do
   sleep 1
 done
-echo "Done"
+echo "It's done. You can continue"
